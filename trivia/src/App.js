1eb9fb72ec4f61a7b83f777ee1aa61a2734a5399
Furat Aziz <Furat@Furats-MacBook-Pro-2.local>
2020-07-16 19:50:35 -0400

    Added fetch for trivia API

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './Styles/App.css';
+import React, { useEffect } from 'react';
+import './App.css';
 
 const App = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect( () => {
+    fetch('https://opentdb.com/api.php?amount=10&type=boolean')
+      .then(response => response.json)
+      .then(data => setData(data.results))
+  }
+);
+
   return (
     <div>
       <h1>WE HAVE BEGUN</h1>
     </div>
   );
-}
+};
 
 export default App;
