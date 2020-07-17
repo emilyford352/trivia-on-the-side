ac9333aef308e1019e6840b70f5b05a3ae08f034
emilyford352 <emilyford352@gmail.com>
2020-07-16 20:16:41 -0400

    Adding loading state yo

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import './Styles/App.css';
+import Loading from './Components/Loading';
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect( () => {
+    setLoading(true);
     fetch('https://opentdb.com/api.php?amount=10&type=boolean')
       .then(response => response.json())
-      .then(data => setData(data.results))
+      .then(data => {
+          setData(data.results);
+          setLoading(false);
+      })
   }, []);
 
   return (
     <div>
+        {loading ? <Loading/> :
+            data.length > 0 && data[0].category
+        }
       <h1>WE HAVE BEGUN</h1>
-        {data.length > 0 && data[0].category}
     </div>
   );
 };
