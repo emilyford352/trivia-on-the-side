c597bf8e4c282f5d62168f8958e64e0018547fdf
emilyford352 <emilyford352@gmail.com>
2020-07-16 20:11:51 -0400

    Fixing useeffect/api call

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,20 +1,19 @@
-import React, { useEffect } from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './Styles/App.css';
 
 const App = () => {
-
   const [data, setData] = useState([]);
 
   useEffect( () => {
     fetch('https://opentdb.com/api.php?amount=10&type=boolean')
-      .then(response => response.json)
+      .then(response => response.json())
       .then(data => setData(data.results))
-  }
-);
+  }, []);
 
   return (
     <div>
       <h1>WE HAVE BEGUN</h1>
+        {data.length > 0 && data[0].category}
     </div>
   );
 };
